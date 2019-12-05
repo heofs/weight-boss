474e1b680a30e61d8908ca2780eb42098540f3b4
Henning Ofstad <ofstad10@gmail.com>
2019-12-05 08:20:12 +0000

    Add loading state

--- frontend/src/utils/authentication.js
+++ frontend/src/utils/authentication.js
@@ -11,32 +11,42 @@ export const AuthProvider = ({ children }) => {
 
 function useFirebaseAuth() {
   const [user, setUser] = useState(null);
-  const auth = firebase.auth();
+  const [loading, setLoading] = useState(true);
+
   const googleProvider = new firebase.auth.GoogleAuthProvider();
 
   const signin = (email, password) => {
-    return auth.signInWithEmailAndPassword(email, password).then((response) => {
-      setUser(response.user);
-      return response.user;
-    });
+    return firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then((response) => {
+        setUser(response.user);
+        return response.user;
+      });
   };
 
   const signup = (email, password) => {
-    return auth.createUserWithEmailAndPassword(email, password).then((res) => {
-      const user = res.user;
-      setUser(user);
-    });
+    return firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then((res) => {
+        const user = res.user;
+        setUser(user);
+      });
   };
 
   const persistSignin = () => {
-    return auth.setPersistence(firebase.auth.Auth.Persistence.LOCAL);
+    return firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL);
   };
 
   const signout = () => {
-    return auth.signOut().then(() => {
-      setUser(false);
-      localforage.setItem('weightData', []);
-    });
+    return firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        setUser(false);
+        localforage.setItem('weightData', []);
+      });
   };
 
   const signinGoogle = async () => {
@@ -53,27 +63,30 @@ function useFirebaseAuth() {
   };
 
   const sendPasswordResetEmail = (email) => {
-    return auth.sendPasswordResetEmail(email);
+    return firebase.auth().sendPasswordResetEmail(email);
   };
 
   const confirmPasswordReset = (code, password) => {
-    return auth.confirmPasswordReset(code, password);
+    return firebase.auth().confirmPasswordReset(code, password);
   };
 
   useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((user) => {
+    const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      setLoading(false);
       if (user) {
         setUser(user);
       } else {
         setUser(false);
       }
     });
+
     // Cleanup subscription on unmount
     return () => unsubscribe();
-  }, [auth]);
+  }, []);
 
   return {
     user,
+    loading,
     signin,
     signup,
     persistSignin,
