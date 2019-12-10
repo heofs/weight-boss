bee5ad216e01581d1f486ce63dd90118a8dce547
Henning Ofstad <ofstad10@gmail.com>
2019-12-10 08:11:13 +0000

    Move variables to env

--- frontend/src/utils/firebase.js
+++ frontend/src/utils/firebase.js
@@ -1,15 +1,15 @@
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
 
-// Set these to your own variables.
+const API_KEY = process.env.REACT_APP_API_KEY;
+const PROJECT_ID = process.env.REACT_APP_PROJECT_ID;
+
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: 'weight-boss.firebaseapp.com',
-  databaseURL: 'https://weight-boss.firebaseio.com',
-  projectId: 'weight-boss',
-  storageBucket: 'weight-boss.appspot.com',
-  messagingSenderId: '156485627213',
-  appId: '1:156485627213:web:a8664bba782e8a64285b04',
+  apiKey: API_KEY,
+  authDomain: `${PROJECT_ID}.firebaseapp.com`,
+  databaseURL: `https://${PROJECT_ID}.firebaseio.com`,
+  projectId: PROJECT_ID,
+  storageBucket: `${PROJECT_ID}.appspot.com`,
 };
 
 firebase.initializeApp(firebaseConfig);
