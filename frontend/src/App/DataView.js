fa9c89f4bddbebe4f41638c0da20b0e1aa968793
Henning Ofstad <ofstad10@gmail.com>
2020-02-14 18:43:55 +0000

    Fix loading states

--- frontend/src/App/DataView.js
+++ frontend/src/App/DataView.js
@@ -1,8 +1,20 @@
 import React from 'react';
+
 import Graph from './Graph';
 import DataTable from './DataTable';
 
+import { useDatabase } from 'enhancers/useDatabase';
+
 const DataView = () => {
+  const { loading, weightData } = useDatabase();
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (!weightData.length) {
+    return <p>Add weight to show history.</p>;
+  }
+
   return (
     <>
       <Graph height={300} width={500} />
