e9eb8c34a4f22c892ad0e2ad8d6fae0d398efae0
Henning Ofstad <ofstad10@gmail.com>
2020-05-23 22:29:05 +0200

    Add prettier and eslint configs

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,3 +1,37 @@
 module.exports = {
-    "extends": "airbnb"
+  plugins: ['prettier', 'jest'],
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+    'plugin:jest/recommended',
+  ],
+  rules: {
+    // 'new-cap': [2, { capIsNewExceptions: ['List', 'Map', 'Set'] }],
+    // 'import/default': 0,
+    // 'import/no-duplicates': 0,
+    // 'import/named': 0,
+    // 'import/namespace': 0,
+    'import/no-unresolved': 0,
+    // 'import/no-named-as-default': 2,
+    'import/no-extraneous-dependencies': 0,
+    'arrow-body-style': 0,
+    // indent: [2, 2, { SwitchCase: 1 }],
+    // 'no-alert': 0,
+    // 'linebreak-style': 0,
+    // 'react/no-multi-comp': 0,
+    'react/jsx-props-no-spreading': 0,
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'prettier/prettier': 'error',
+  },
+  parserOptions: {
+    sourceType: 'module',
+  },
+  env: {
+    es6: true,
+    node: true,
+    browser: true,
+  },
 };
