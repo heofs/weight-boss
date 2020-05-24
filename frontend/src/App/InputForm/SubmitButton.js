460385f9a6e7be567ce33d6f38be192798eb2c24
Henning Ofstad <ofstad10@gmail.com>
2020-05-24 22:11:30 +0200

    Animate submit button

--- frontend/src/App/InputForm/SubmitButton.js
+++ frontend/src/App/InputForm/SubmitButton.js
@@ -4,16 +4,68 @@ import styled from 'styled-components';
 import { colors } from 'constants/theme';
 import BaseButton from 'components/Buttons/BaseButton';
 
+import { ReactComponent as SpinnerIcon } from 'images/spinner.svg';
+import { ReactComponent as CheckedIcon } from 'images/checked.svg';
+
 const Button = styled(BaseButton)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
   text-transform: uppercase;
-  margin: 2rem 0em;
-  padding: 1em 2em;
+  margin: 2rem auto;
+  padding: 0.5em 2em;
   width: 65%;
+  height: 3em;
   font-weight: 500;
   ::-moz-focus-inner {
     border: 0;
   }
-  ${(props) => props.warning && `color: ${colors.danger};`};
+
+  svg {
+    height: 1em;
+    padding: 0;
+  }
+`;
+
+const LoadingButton = styled(Button)`
+  background-color: ${colors.darkPrimary};
+  :hover {
+    background-color: ${colors.darkPrimary};
+    cursor: default;
+  }
+  svg {
+    animation: loading 1s linear infinite;
+  }
+`;
+
+const FinishedButton = styled(Button)`
+  background-color: ${(props) => props.color};
+  :hover {
+    background-color: ${(props) => props.color};
+    cursor: default;
+  }
+
+  span {
+    display: block !important;
+    font-size: 24px;
+    animation: scale 0.5s linear;
+
+    i {
+      transform-origin: center center;
+    }
+  }
+
+  svg {
+    height: 0.8em;
+  }
+`;
+const ErrorButton = styled(Button)`
+  color: black;
+  background-color: ${(props) => props.color};
+  :hover {
+    background-color: ${(props) => props.color};
+    cursor: default;
+  }
 `;
 
 const SubmitButton = ({
@@ -24,15 +76,17 @@ const SubmitButton = ({
   ...otherProps
 }) => {
   const [state, setState] = useState('ready');
+  const [text, setText] = useState('');
 
   const onClick = () => {
-    if (weight === '' || weight.includes(',')) {
+    if (weight === '') {
       setState('error');
+      setText('Enter weight');
       return;
     }
     setState('loading');
     addWeight(weight, dateTime.getTime()).then(() => {
-      setState('ready');
+      setState('finished');
     });
   };
 
@@ -41,16 +95,25 @@ const SubmitButton = ({
   switch (state) {
     case 'loading':
       return (
-        <Button {...otherProps} disabled>
-          Saving...
-        </Button>
+        <LoadingButton {...otherProps} disabled color="#57cc99">
+          <SpinnerIcon />
+        </LoadingButton>
+      );
+    case 'finished':
+      setReady();
+      return (
+        <FinishedButton {...otherProps} disabled color="#3bb273">
+          <span>
+            <CheckedIcon />
+          </span>
+        </FinishedButton>
       );
     case 'error':
       setReady();
       return (
-        <Button {...otherProps} disabled warning>
-          Enter weight correctly
-        </Button>
+        <ErrorButton {...otherProps} disabled color="#f95738">
+          {text}
+        </ErrorButton>
       );
     default:
       return (
